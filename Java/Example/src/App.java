bd289a758ec37c199d08e60cc864bf6a4a5af5b7
Nathan Robinson <NRobinson@rcs-uk.com>
2024-05-09 13:10:38 +0100

    Java

---
+++ Java/Example/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        var cal=new Calculator();
+        System.out.println(cal.Read());
+        cal.Print();
+
+        cal.Add(1).Add(2).Print();
+        cal.Multiply(2.5).Print();
+        cal.Sub(1.5).Print();
+        cal.Div(0).Print();
+    }
+}
---
+++ Java/Example/src/Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+    private double Ans;
+    public Calculator(){
+        this.Ans=0;
+    }
+    public Calculator Add(double input){
+        this.Ans += input; return this;
+    }
+    public Calculator Sub(double input) {
+        this.Ans -= input; return this;
+    }
+    public Calculator Multiply(double input) {
+        this.Ans *= input; return this;
+    }
+    public Calculator Div(double input) {
+        this.Ans/=input; return this;
+    }
+    public double Read(){
+        return this.Ans;
+    }
+    public void Print(){System.out.println(String.format("Ans currently at: %f", this.Ans));}
+}
